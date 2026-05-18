a867c323e69fee5949bfced98ac43a9f185123b8
Moise <moisemichaud@gmail.com>
2026-05-18 13:14:01 -0400

    worked on project using OOP classes

---
+++ components/FormValidator.js
@@ -0,0 +1,78 @@
+// components/FormValidator.js
+
+export class FormValidator {
+  constructor(settings, formElement) {
+    this._settings = settings;
+    this._formElement = formElement;
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._settings.inputSelector),
+    );
+    this._submitButton = this._formElement.querySelector(
+      this._settings.submitButtonSelector,
+    );
+  }
+
+  _showInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`,
+    );
+    inputElement.classList.add(this._settings.inputErrorClass);
+    errorElement.textContent = inputElement.validationMessage;
+    errorElement.classList.add(this._settings.errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`,
+    );
+    inputElement.classList.remove(this._settings.inputErrorClass);
+    errorElement.classList.remove(this._settings.errorClass);
+    errorElement.textContent = "";
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._submitButton.classList.add(this._settings.inactiveButtonClass);
+      this._submitButton.disabled = true;
+    } else {
+      this._submitButton.classList.remove(this._settings.inactiveButtonClass);
+      this._submitButton.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+    this._toggleButtonState();
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+
+  resetValidation() {
+    this._formElement.reset();
+    this._toggleButtonState();
+  }
+}
---
+++ components/Todo.js
@@ -0,0 +1,60 @@
+// components/Todo.js
+
+export class Todo {
+  constructor(data, selector) {
+    this._name = data.name;
+    this._completed = data.completed;
+    this._date = data.date;
+    this._id = data.id;
+    this._selector = selector;
+  }
+
+  _getTemplate() {
+    const todoTemplate = document
+      .querySelector(this._selector)
+      .content.querySelector(".todo")
+      .cloneNode(true);
+
+    return todoTemplate;
+  }
+
+  _setEventListeners() {
+    this._deleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+
+    this._checkbox.addEventListener("change", () => {
+      this._completed = !this._completed;
+    });
+  }
+
+  _generateDate() {
+    const dueDate = new Date(this._date);
+    if (!isNaN(dueDate)) {
+      this._dateEl.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
+
+  getView() {
+    this._todoElement = this._getTemplate();
+
+    this._dateEl = this._todoElement.querySelector(".todo__date");
+    this._checkbox = this._todoElement.querySelector(".todo__completed");
+    this._todoElement.querySelector(".todo__name").textContent = this._name;
+    this._deleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    const todoLabel = this._todoElement.querySelector(".todo__label");
+    this._checkbox.id = `todo-${this._id}`;
+    todoLabel.setAttribute("for", `todo-${this._id}`);
+    this._checkbox.checked = this._completed;
+
+    this._generateDate();
+    this._setEventListeners();
+
+    return this._todoElement;
+  }
+}
